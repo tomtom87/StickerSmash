d9dbd4c12e018f3336cbe6be898b47a993070a22
Tom Whitbread <tom@gript.co.uk>
2025-02-09 18:29:02 +0700

    adding components and button file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,41 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from 'expo-router';
 
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-    <Text style={styles.text}>Home screen</Text>
-    <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+    <View style={styles.imageContainer}>
+      <ImageViewer imgSource={PlaceholderImage} />
+    </View>
+    <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
   </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
